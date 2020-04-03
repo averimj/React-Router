a6799a2129c458afbeaced9a26208d7de7346478
Averi Johnson <averimj@live.com>
2020-04-02 19:51:25 -0600

    Updates

---
+++ precache-manifest.be659a071551b64fa5676bc2a2a7534e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5268732f3b08e37768cc69d020f06b01",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "9b5af63809ab212f3cfb",
+    "url": "/react-router/static/css/main.4ad691b2.chunk.css"
+  },
+  {
+    "revision": "95f72255d95b70abd267",
+    "url": "/react-router/static/js/2.4c0913a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router/static/js/2.4c0913a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5af63809ab212f3cfb",
+    "url": "/react-router/static/js/main.15b9be9d.chunk.js"
+  },
+  {
+    "revision": "583c756a21653c96bb62",
+    "url": "/react-router/static/js/runtime-main.64e4d405.js"
+  }
+]);
